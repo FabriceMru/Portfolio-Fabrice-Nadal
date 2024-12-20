50f155a06fbff7f0b576781f40d7a0446ef69f83
FabriceMru <jean.nadal@dci-student.org>
2024-12-20 07:10:05 +0100

    Website build

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,46 @@
+import { socials } from "../constants/index.jsx";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container py-10">
+        <div className="flex w-full max-md:flex-col">
+          <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
+            <p className="opacity-70">Copyright, Fabrice Nadal</p>
+          </div>
+          <div className="flex items-center justify-center sm:ml-auto">
+            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+              {" "}
+            </p>
+            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+              E-Mail :
+            </p>
+            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+              <a
+                href="mailto:fabrice-nadal@protonmail.com"
+                className="hover:underline"
+              >
+                fabrice-nadal@protonmail.com
+              </a>
+            </p>
+          </div>
+
+          <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
+            {socials.map(({ id, url, icon, title }) => (
+              <li key={id}>
+                <a href={url} className="social-icon">
+                  <img
+                    src={icon}
+                    alt={title}
+                    className="size-1/3 object-contain"
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,37 @@
+import { Element, Link as LinkScroll } from "react-scroll";
+import Button from "../components/Button.jsx";
+
+const Hero = () => {
+  return (
+    <section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+      <Element name="hero">
+        <div className="container">
+          <div className="relative z-2 max-w-512 max-lg:max-w-388">
+            <div className="caption small-2 uppercase text-p3">About me</div>
+            <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
+              "LEIDENSCHAFTLICH, INNOVATIV
+            </h1>
+            <p className="max-w-440 mb-14 body-1 max-md:mb-10">
+              "Als Full-Stack Entwickler verbinde ich technische Expertise mit
+              strategischem Verständnis, um digitale Lösungen zu schaffen, die
+              Menschen begeistern."
+            </p>
+            <LinkScroll to="features" offset={-100} spy smooth>
+              <Button icon="/images/zap.svg">Qualifikationen</Button>
+            </LinkScroll>
+          </div>
+
+          <div className="flex-2 ml-8 absolute left-[calc(75%-400px)] top-[calc(86%-400px)]">
+            <img
+              src="/images/screens-me.png"
+              className="w-full max-w-[400px] h-auto rounded-md"
+              alt="hero"
+            />
+          </div>
+        </div>
+      </Element>
+    </section>
+  );
+};
+
+export default Hero;
