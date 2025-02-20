73982b704400028302b5c45228161cb57f6e9e93
FabriceMru <jean.nadal@dci-student.org>
2025-02-20 12:46:55 +0100

    adjusted to medium and small screens responivness

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,49 +1,48 @@
 import { Element, Link as LinkScroll } from "react-scroll";
-import Button from "../components/Button.jsx";
 
 const Hero = () => {
   return (
-    <section className="relative pt-60 pb-10 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-md:pb-32">
+    <section className="relative  pt-32 pb-20">
       <Element name="hero">
-        <div className="container">
-          <div className="relative z-2 max-w-512 max-lg:max-w-388">
-            <div className="caption small-2 uppercase text-p3">About me</div>
-            <h1 className="mb-4 h1 text-p1 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-4xl max-md:leading-12">
-              LEIDENSCHAFTLICH, INNOVATIV
-            </h1>
-
-            {/* Bild nur in der mobilen Ansicht */}
-            <div className="my-6 md:hidden">
-              <img
-                src="/images/Fabrice_autumn.png"
-                className="w-full h-auto max-w-full rounded-md"
-                alt="Foto von Fabrice Nadal im Herbst"
-              />
+        <div className="container mx-auto px-4">
+          <span className="mt-22 mb-8 block text-sm font-medium uppercase tracking-[0.2em] caption small-2 text-p3">
+            About me
+          </span>
+          <div className="flex flex-col items-start lg:flex-row lg:items-start lg:justify-between lg:space-x-8">
+            {/* Text Content */}
+            <div className="w-full lg:w-1/2">
+              <h1 className="text-5xl font-bold h0 text-p1 lg:text-6xl xl:text-7xl ">
+                INNOVATION
+                <br />
+                DURCH
+                <br />
+                ENTWICKLUNG
+              </h1>
+              <div className="mt-8 space-y-6 text-gray-300">
+                <p className="text-xl font-medium leading-relaxed">
+                  Als Junior Full Stack Entwickler am Anfang meiner Karriere
+                  bringe ich frische Perspektiven in die Technologiebranche.
+                  Meine Stärke liegt in der Verbindung von technischer Präzision
+                  und innovativem Denken, sowie das zusammenspiel neuster
+                  Technologien.
+                </p>
+                <p className="text-xl font-medium leading-relaxed">
+                  Ich lebe für die Entwicklung digitaler Lösungen, die neue
+                  Standards setzen!
+                </p>
+              </div>
             </div>
 
-            <p className="max-w-440 mb-9 body-1 max-md:mb-10">
-              "Als Junior Full Stack Entwickler verbinde ich technische
-              Expertise mit strategischem Verständnis, um digitale Lösungen zu
-              schaffen, die Menschen begeistern.
-              <br />
-              <br />
-              Mit großer Wissbegierde und Motivation entwickle ich mich
-              kontinuierlich weiter und bleibe stets auf dem neuesten Stand der
-              Technologie."
-              <br />
-              <br />
-              Lassen Sie uns sprechen und nehmen Sie gerne Kontakt mit mir auf!
-            </p>
-            <LinkScroll to="features" offset={-100} spy smooth></LinkScroll>
-          </div>
-
-          {/* Bild für größere Bildschirme */}
-          <div className="flex-2 ml-2 absolute left-[calc(81.5%-600px)] top-[calc(74.5%-400px)] w-full my-11 hidden md:block">
-            <img
-              src="/images/Fabrice_autumn.png"
-              className="w-full h-auto max-w-full md:max-w-[550px] rounded-md"
-              alt="Foto von Fabrice Nadal im Herbst"
-            />
+            {/* Image */}
+            <div className="mt-12 w-full lg:mt-0 lg:w-1/2">
+              <div className="relative mx-auto lg:mx-0">
+                <img
+                  src="/images/Fabrice_autumn.png"
+                  alt="Fabrice Nadal im Herbst"
+                  className="h-auto w-full max-w-lg object-cover rounded-[2rem]"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </Element>
